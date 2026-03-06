ff8c1b61078c5b1bcc4edcee2272a593eca93c45
John Farrell <johnjfarrell20@gmail.com>
2026-03-05 16:00:30 -0800

    Wire main.go router, static embed, logging middleware

--- interfaces/middleware/logging.go
+++ interfaces/middleware/logging.go
@@ -1,1 +1,36 @@
 package middleware
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.uber.org/zap"
+)
+
+// Logging returns a gorilla/mux middleware that logs each request.
+func Logging(log *zap.Logger) mux.MiddlewareFunc {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			rw := &responseWriter{ResponseWriter: w, code: http.StatusOK}
+			next.ServeHTTP(rw, r)
+			log.Info("request",
+				zap.String("method", r.Method),
+				zap.String("path", r.URL.Path),
+				zap.Int("status", rw.code),
+				zap.Duration("duration", time.Since(start)),
+			)
+		})
+	}
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.code = code
+	rw.ResponseWriter.WriteHeader(code)
+}
